f72a83202f37c85b3b82bdc671d01525fe81b925
italo <you@example.com>
2022-02-23 12:36:38 -0300

    teste

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+const foo = 'bar'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ fontFamily: 'monospace' }}>
+        foo; <br /> {foo};
+      </h1>
+    </div>
+  )
+}
